12f7c99e9f19df4e77c061c6bb967f8fe2947c93
Tomáš Brada <tomasbrod@azet.sk>
2015-04-01 21:46:30 +0200

    Sedimentary layer rewrite.

--- init.lua
+++ init.lua
@@ -7,36 +7,17 @@ if (minetest.get_modpath("intllib")) then
 	S = function ( s ) return s end
 end
 
+rocks={}
+
 local modpath=minetest.get_modpath(minetest.get_current_modname())
 
-dofile(modpath.."/register.lua")
+print("[rocks] begin")
 
-rocks.noiseparams_layers = {
-        offset = 0,
-        scale = 1,
-        spread = {x=80, y=80, z=80},
-        octaves = 2,
-        persist = 0.7
-}
-
-dofile(modpath.."/mapgen.lua")
---dofile(modpath.."/testing.lua")
-
-print("[rocks] core loaded.")
-
-dofile(modpath.."/geologicaLayers.lua")
-dofile(modpath.."/geologicaStrata.lua")
-dofile(modpath.."/geologicaVeins.lua")
+dofile(modpath.."/sed.lua")
 
 minetest.register_on_mapgen_init(function(mapgen_params)
- 
- for i,d in pairs(rocks.layers_name) do table.insert(rocks.layers,d) end
- table.sort(rocks.layers,function(a,b)
-  return a.height<b.height
- end)
- 
  -- todo: disable caves and ores
-
- print("[rocks] mapgen initalized ("..#rocks.layers.." layers)")
-
+ print("[rocks] mapgen initalized ")
 end)
+
+print("[rocks] done")
